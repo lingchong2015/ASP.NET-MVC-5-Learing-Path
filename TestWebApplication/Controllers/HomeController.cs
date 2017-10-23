b8d07e1377e832595cdf4ebe57efc7f33e880ca1
linchong <linchong@DESKTOP-4H0MEN4>
2017-10-23 23:45:44 +0800

    添加项目文件。

---
+++ TestWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Web.Mvc;
+
+namespace TestWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        //[ChildActionOnly]
+        public ActionResult ContactPartial()
+        {
+            ViewBag.Message = "It's partial view.";
+            
+            return PartialView(new Product() { Name = "Mac Book Pro" });
+        }
+
+        public class Product
+        {
+            public string Name { get; set; }
+        }
+    }
+}
